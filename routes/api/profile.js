e6fe3fa8a1c1ad6d440499129ad042dc4f9f0871
PremanandChowdhuy <freudigprem@gmail.com>
2021-01-13 23:58:08 +0530

    Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -179,4 +179,38 @@ router.delete('/', auth, async(req, res) => {
    } 
 });
 
+
+// @route   PUT api/profile/experience
+// @desc    Add Profile Experience
+// @access  Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company name is required').not().isEmpty(),
+    check('from', 'From date is required')
+]], async(req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {title, company, location, from, to, current, description} = req.body;
+
+    const newExp = { title, company, location, from, to, current, description};
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experience.unshift(newExp);
+        
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+       console.log(error.message);
+       res.status(500).send('Server Error');
+    }
+
+});
+
 module.exports = router;

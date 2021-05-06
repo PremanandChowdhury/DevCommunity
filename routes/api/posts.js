cbb965cdfb20daca8217e43618a3f44873b37d7c
PremanandChowdhuy <freudigprem@gmail.com>
2021-05-06 21:15:52 +0530

    Added add post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/posts
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Posts Route'))
+const Post = require('../../models/Post')
+const User = require('../../models/User')
+
+// @route   POST api/posts
+// @desc    Add Post
+// @access  Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password')
+
+      const newPost = new Post({
+        text: req.body.text,
+        avatar: user.avatar,
+        name: user.name,
+        user: req.user.id,
+      })
+
+      const post = await newPost.save()
+
+      res.json(post)
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 module.exports = router
